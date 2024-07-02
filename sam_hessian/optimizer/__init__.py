64a79dcb4c279cf6259977be32261cd31a4af142
lhchau <lhchau20@apcs.fitus.edu.vn>
2024-07-02 10:23:06 +0700

    samckpt123

--- sam_hessian/optimizer/__init__.py
+++ sam_hessian/optimizer/__init__.py
@@ -13,6 +13,7 @@ from .samckpt4 import SAMCKPT4
 from .samckpt12 import SAMCKPT12
 from .samckpt13 import SAMCKPT13
 from .samckpt14 import SAMCKPT14
+from .samckpt123 import SAMCKPT123
 from .samckpt134 import SAMCKPT134
 from .shame import SHAME
 from .same import SAME
@@ -94,6 +95,11 @@ def get_optimizer(
         )
     elif opt_name == 'samckpt14':
         return SAMCKPT14(
+            net.parameters(),
+            **opt_hyperparameter
+        )
+    elif opt_name == 'samckpt123':
+        return SAMCKPT123(
             net.parameters(),
             **opt_hyperparameter
         )
